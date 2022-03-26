4e870cb5794f85af8ee914ba4f146310456aa543
aratar79 <aratar79@gmail.com>
2022-03-26 19:33:36 +0100

    modularize app

---
+++ src/app/gifs/gifs-result/gifs-result.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gifs-result',
+  templateUrl: './gifs-result.component.html',
+  styleUrls: ['./gifs-result.component.scss']
+})
+export class GifsResultComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
